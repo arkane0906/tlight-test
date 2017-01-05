15bff832c5428aa66f62ada31997b5c45034e68a
Eugen Idelbaev <arkane0906@gmail.com>
2017-01-05 19:07:51 +0300

    Create connection to MySQL Database

--- tlight/settings.py
+++ tlight/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'tlight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tlight',
+        'USER': 'arkane',
+        'PASSWORD': '0906422',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
